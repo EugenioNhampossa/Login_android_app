3a22dce46f4cd2e660e07337fb23e5b618783443
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-05-07 21:04:04 +0200

    chore: add ResetPassword and TokenVerification creens

---
+++ app/src/main/java/mz/ac/isutc/i31/logintutorial/ResetPassword.java
@@ -0,0 +1,14 @@
+package mz.ac.isutc.i31.logintutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
